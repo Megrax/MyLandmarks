4cde765133950babf8677d7114a5321cd6c4cc77
Megrax <geronimooo@foxmail.com>
2021-01-14 18:14:37 +0800

    init

---
+++ LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Geronimo on 2021/1/12.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
