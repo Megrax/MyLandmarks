78fad0cc4c64f2b6a3f4283e764a70cf9c7aab40
Megrax <geronimooo@foxmail.com>
2021-01-15 18:31:23 +0800

    update

--- Views/LandmarkList.swift
+++ Views/LandmarkList.swift
@@ -22,6 +22,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 8", "iPhone XS Max", "iPad Air (3rd generation)"], id: \.self) {
+            deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
